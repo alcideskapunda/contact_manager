2fd05b30c71f192a2ca5f0aecddcfb1d26e16cec
Antero Kapunda <alcideskapunda@gmail.com>
2023-12-10 11:58:48 +0100

    a parte de remover já esta feita, só falta endereitara questão de editar porque esta a editar todos em vez de só um

--- js/main.js
+++ js/main.js
@@ -4,6 +4,14 @@ const addTaskButton = document.querySelector(".add-new-contact");
 const filterContact = document.querySelector(".cod-country");
 const taksContainer = document.querySelector("#tasks-container");
 
+const inputEdit = document.querySelector(".input-edit-contact");
+const closeEdit = document.querySelector("#close-modal");
+const modal = document.querySelector("#modal");
+const fade = document.querySelector("#fade");
+
+const contactEditBtn = document.querySelector("#contactoEditado");
+//let contAntesDeSerEditado;
+
 
 const validateContact = () => {
     const angolaRegex = /^\+\d{3}\d{3}\d{3}\d{3}$/;
@@ -30,21 +38,44 @@ const getAddContact = () => {
 
     const pContact = document.createElement("p");
     pContact.innerText = inputContact.value;
+    /*contactEditBtn.addEventListener("click", (e) => {
+        pContact.innerText = inputEdit.value;
+        e.addEventListener("click", () => toggleModal());
+    });*/
     divContact.appendChild(pContact);
     
     const btnEdit = document.createElement("button");
     btnEdit.classList.add("editBtn");
     btnEdit.innerHTML = '<i class="fa fa-pencil"></i>';
+    [btnEdit, closeEdit, contactEditBtn].forEach((el) => {
+        el.addEventListener("click", () => toggleModal());
+    });
+    /*btnEdit.addEventListener("click", (e) => {
+        const targetEl = e.target;
+        const parentEl = targetEl.closest("div");
+        if (parentEl && parentEl.querySelector("p")) {
+            inputEdit.value = parentEl.querySelector("p").innerText || "";
+        }
+    });*/
     divContact.appendChild(btnEdit);
     
     const btnDelete = document.createElement("button");
     btnDelete.classList.add("deleteBtn");
     btnDelete.innerHTML = '<i class="fa fa-trash"></i>';
+    btnDelete.addEventListener("click", (e) => {
+        const targetEl = e.target;
+        const parentEl = targetEl.closest("div");
+        parentEl.remove();
+    });
     divContact.appendChild(btnDelete);
     
     taksContainer.appendChild(divContact);
     
     inputContact.value = "";
+}
+
+const toggleModal = () => {
+    [modal, fade].forEach((el) => el.classList.toggle("hide"));
 }
 
 const inputChange = () => {
@@ -55,5 +86,30 @@ const inputChange = () => {
     }
 };
 
+/*closeEdit.addEventListener("click", () => {
+    [closeEdit, contactEditBtn].forEach((el) => {
+        el.addEventListener("click", () => toggleModal());
+    });
+});*/
+
 addTaskButton.addEventListener("click", () => getAddContact());
 inputContact.addEventListener("change", () => inputChange());
+
+
+/*document.addEventListener("click", (e) => {
+    const targetEl = e.target;
+    const parentEl = targetEl.closest("div");
+    //let contacto;
+
+    if (parentEl && parentEl.querySelector("p")) {
+        inputEdit.value = parentEl.querySelector("p").innerText || "";
+    }
+
+    if (targetEl.classList.contains("contactoEditado")) {
+        console.log("editou");
+
+        inputEdit.value = inputContact.value;
+        //contAntesDeSerEditado.value = inputContact.value;
+    }
+    
+});*/
